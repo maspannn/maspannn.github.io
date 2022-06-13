11f986a31a409a4b926e23e2a12fc7d124455850
maspannn <cahbagus080@gmail.com>
2022-06-13 19:08:45 +0700

    Mas Pannn

--- assets/produk.js
+++ assets/produk.js
@@ -1,101 +1,164 @@
-var LoadMore = function(userOptions) {
-  this.options = {
-    "pageSize": 6,
-    "dataUrl": "",
-    "container": "#list-produk",
-    "triggerText": "Produk Lainnya",
-    "triggerLoadingText": "Loading...",
-    "trigger": "#produk-lainnya",
-    "callback": null
-  };
-  $.extend(this.options, userOptions);
-  this._index = 0;
-  this._itemsCurrentlyDisplayed = 0;
-};
+var data = [
+	{
+        "nama": "Benih mentimun hibrida F1 MIRA dari PT.AGRI MAKMUR PERTIWI. isi 25gram",
+        "kategori": "Benih",
+        "deskripsi": "Buah berwarna  hijau gelap dan tidak pahit. Bentuk buah silindris, panjang 22 – 24 cm dan diameter ± 5 cm. Jumlah buah per tanaman 14 – 18, berat buah 250 – 300 gram. Potensi hasil bisa mencapai 52,5 – 75,6 ton/ha. Dapat dipanen mulai umur 33 – 35 hari setelah pindah tanam. Lebih tahan virus.",
+        "link": "https://shopee.co.id/product/231758580/4619820005?smtt=0.99855215-1654831771.9",
+        "foto": "https://cf.shopee.co.id/file/32e85d930ae5924cbe917c763bd58896",
+        "harga":"55.000"
+    },
+    {
+        "nama": "Fungisida Sistemik DIMORF 500 SC . Isi 250ml dari Mitra Agro Sukses",
+        "kategori": "Fungisida",
+        "deskripsi": "Fungisida Sistemik DIMORF 500 SC . Isi 250ml dari Mitra Agro Sukses. Fungisida sistemik berbentuk pekatan suspensi berwarna putih untuk mengendalikan penyakit bulai, penyakit busuk daun atau akar pada tanaman dengan cara perlakuan benih ataupun dengan spray.",
+        "link": "https://shopee.co.id/Fungisida-Sistemik-DIMORF-500-SC-.-Isi-250ml-dari-Mitra-Agro-Sukses-i.231758580.3358184511?xptdk=83db757e-72bb-4163-821c-350ccb752b16",
+        "foto": "https://cf.shopee.co.id/file/31300d9ebdf6139871a292b0f3e1306a",
+        "harga":"35.000"
+    },
+    {
+        "nama": "Benih Paria/Pare TANJUNG 11 F1 isi 10gr dari MITRA MERDEKA TANI",
+        "kategori": "Benih",
+        "deskripsi": "Paria f1 dengan tipe pertumbuhan merambat. Cocok untuk dataran rendah. Pertumbuhan vigor dan sangat produktif. Bentuk buah silindris dengan warana kulit hijau gelap. Permukaan buah bergerigi, daging tebal, rasa buah sedikit pahit. Panjang buah 𝟐𝟎 - 𝟑𝟎 𝐜𝐦, diameter 𝟕 - 𝟖 𝐜𝐦, bobot perbuah 𝟐𝟓𝟎 - 𝟑𝟎𝟎 𝐠𝐫. Panen Umur 𝟒𝟐 𝐇𝐒𝐓 dengan potensi hasil 𝟐𝟓 - 𝟑𝟎 𝐭𝐨𝐧 per hektar.",
+        "link": "https://shopee.co.id/Benih-Paria-Pare-TANJUNG-11-F1-isi-10gr-dari-MITRA-MERDEKA-TANI-i.231758580.8298439898?xptdk=36f95791-a295-480b-b2e7-68b6ebd61b8a",
+        "foto": "https://cf.shopee.co.id/file/ae0ee0a5cd3afdd4f3c9b98bb6665c1d",
+        "harga":"30.000"
+    },
+    {
+        "nama": "Benih unggul gambas hibrida F1 LAMONA dari PERTIWI. Isi 10gram",
+        "kategori": "Benih",
+        "deskripsi": "Warna buah hijau. Pertumbuhan tanaman kokoh dan seragam. Berat buah 200-250 gram dengan potensi hasil banyak.",
+        "link": "https://shopee.co.id/Benih-unggul-gambas-hibrida-F1-LAMONA-dari-PERTIWI.-Isi-10gram-i.231758580.4220325037?smtt=0.99855215-1654831771.9",
+        "foto": "https://cf.shopee.co.id/file/6c5dacc1757898ba94b6921f17f6c1db",
+        "harga":"25.000"
+    },
+    {
+        "nama": "Pupuk NPK Cair Tanigrow",
+        "kategori": "Pupuk",
+        "deskripsi": "Pupuk NPK CAIR TANIGROW adalah pupuk daun, bunga dan buah yang sangat efektif untuk merangsang pembentukan bunga dan menghasilkan biji yang bernas terutama pada tanaman kacang-kacangan. Pupuk ini juga mampu melipatgandakan hasil panen tanaman kacang hijau dan kedelai.",
+        "link": "https://saprotan-utama.com/product/tanigrow/",
+        "foto": "https://saprotan-utama.com/wp-content/uploads/2020/11/46_Tanigrow-1-768x768.jpg",
+        "harga":"-"
+    },
+    {
+        "nama": "Alat pelubang/plong plastik mulsa nama bakar diameter 5,5 cm tinggi 70 cm",
+        "kategori": "Alat",
+        "deskripsi": "Alat pelubang/plong plastik mulsa nama bakar diameter 5,5 cm tinggi 70 cm",
+        "link": "https://shopee.co.id/Alat-pelubang-plong-plastik-mulsa-nama-bakar-diameter-5-5-cm-tinggi-70-cm-i.231758580.7782747359?xptdk=28e44b0a-8ad8-4053-8df1-c8892c3771e8",
+        "foto": "https://cf.shopee.co.id/file/04760784fdb325cdf09e58891636087c",
+        "harga":"25.000"
+    },
+    {
+        "nama": "Tapetool, Hand Tying Machine - Alat Pengikat Lanjaran Tanaman",
+        "kategori": "Alat",
+        "deskripsi": "Tapetool adalah alat untuk mengikat tanaman pada ajir (penopang tanaman) atau tali rambatan. Biasanya digunakan untuk tanaman merambat seperti tomat, timun, anggur, dsb. Juga dapat digunakan pada tanaman lain yang membutuhkan penopang.",
+        "link": "https://shopee.co.id/Tapetool-Hand-Tying-Machine-Alat-Pengikat-Lanjaran-Tanaman-i.175498478.2782789037?sp_atk=f13a1cc0-4ac8-4a26-a416-2af1fcb0d632&xptdk=f13a1cc0-4ac8-4a26-a416-2af1fcb0d632",
+        "foto": "fotos/thumb/bali.jpg",
+        "harga":"130.000"
+    },
+    {
+        "nama": "ROUNDUP 486SL HERBISIDA OBAT PEMBASMI RUMPUT LIAR & GULMA 200 ML",
+        "kategori": "Pestisida",
+        "deskripsi": "ROUNDUP 486 SL adalah herbisida populer dimana diformulasi dalam bentuk larutan yang mudah larut dalam air yang dapat mengendalikan gulma berdaun sempit, berdaun lebar dan teki-tekian. Diformulasikan dengan menggunakan teknologi biosorb.",
+        "link": "https://shopee.co.id/ROUNDUP-486SL-HERBISIDA-OBAT-PEMBASMI-RUMPUT-LIAR-GULMA-200-ML-i.16443162.6009901425?xptdk=e2f3ac36-e2a7-4bd2-bdf9-2dce53671e8d",
+        "foto": "https://cf.shopee.co.id/file/9dc6d7425c53838f1241b12dc057d4f1",
+        "harga":"33.000"
+    },
+    {
+        "nama": "GRAMOXONE 276SL HERBISIDA RACUN PEMBASMI RUMPUT & GULMA 250 ML",
+        "kategori": "Herbisida",
+        "deskripsi": "GRAMOXONE 276SL 250 ML. Bahan Aktif: Parakuat Diklorida 276 g. Bentuk: larutan warna hijau tua (bersifat kontak). Diproduksi oleh : PT. SYNGENTA INDONESIA.",
+        "link": "https://shopee.co.id/GRAMOXONE-276SL-HERBISIDA-RACUN-PEMBASMI-RUMPUT-GULMA-250-ML-i.16443162.3910330101?xptdk=bdff72f1-c124-4ddb-9fdd-cd084ce39913",
+        "foto": "https://cf.shopee.co.id/file/34e8eb31f01dfb2e7718ae98274b7c49",
+        "harga":"43.000"
+    }
+];
 
-LoadMore.prototype.scrollToElement = function(selector, time, verticalOffset) {
-  time = typeof(time) != 'undefined' ? time : 1000;
-  verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-  var element = $(selector);
-  var offset = element.offset();
-  var offsetTop = offset.top + verticalOffset;
+var products = "",
+	kategories = "";
 
-};
+for (var i = 0; i < data.length; i++) {
+	var kategori = data[i].kategori,
+		nama = data[i].nama,
+		deskripsi = data[i].deskripsi,
+		harga = data[i].harga,
+		link = data[i].link,
+		foto = data[i].foto;
+	
+	//create product cards
+	products += "<div class='product' data-kategori='" + kategori + "' data-nama='" + nama + "' data-deskripsi='" + deskripsi + "' data-link='" + link + "'><a href='"+ link +"'><div class='product-inner text-left'><img src='" + foto + "' width='100%' height='200px'><br /><div class='nama-produk'>" + nama + "</div><div class='harga-produk'>Rp " + harga + "</div></a></div></div>";
+	
+	//create dropdown of kategories
+	if (kategories.indexOf("<option value='" + kategori + "'>" + kategori + "</option>") == -1) {
+		kategories += "<option value='" + kategori + "'>" + kategori + "</option>";
+	}
+}
 
-LoadMore.prototype.loadData = function() {
-  var self = this;
-  self.triggerFeedback(true);
-  $.getJSON(self.options.dataUrl,
-    function(data) {
-      self.triggerFeedback(false);
-      var totalResults = data.produk.length;
-      var items = [];
-      var dataArr = data.produk.splice(self._index, self.options.pageSize);
-      if (dataArr.length > 0) {
-        $.each(dataArr, function(key, val) {
-          items.push("<li><div class='produk-card shadow'><a href=" + val.link + " target='_blank'><div class='poster-produk'><img id='poster' src=" + val.foto + " alt='' width='100%' height='100%' style='object-fit:cover'></div><div class='nama-produk'><h7 style'text-transform: capitalize;'>" + val.nama + "</h7></div><div class='harga-produk'><p>Rp" + val.harga + "</p></div></a></div></li>");
-        });
-        $(items.join("")).appendTo(self.options.container);
-        var scrollToEl = $(".result").get(self._index);
-        self._index += self.options.pageSize;
-        if (scrollToEl) {
-          // occurs only when not the initial
-          // load of data
-          self.scrollToElement(scrollToEl);
-        }
-        self._itemsCurrentlyDisplayed += dataArr.length;
-        if (self._itemsCurrentlyDisplayed >= totalResults) {
-          self._trigger.hide();
-        }
-        if (self.options.callback != null) {
-          self.options.callback();
-        }
-      }
-    });
-};
+$("#products").html(products);
+$(".filter-kategori").append(kategories);
 
-LoadMore.prototype.triggerFeedback = function(isLoading) {
-  if (isLoading) {
-    this._trigger.text(this.options.triggerLoadingText);
-  } else {
-    this._trigger.text(this.options.triggerText);
-  }
-};
+var filtersObject = {};
 
-LoadMore.prototype.init = function() {
-  var self = this;
-  $(document).ready(
-    function() {
-      self._trigger = $(self.options.trigger);
-      self.loadData();
-      self._trigger.on("click", function() {
-        self.loadData();
-      });
-    });
-};
-
-// example usage
-var loadMore = new LoadMore({
-  "dataUrl": "https://raw.githubusercontent.com/maspannn/maspannn.github.io/master/produk.json",
-  "pageSize": 6
+//on filter change
+$(".filter").on("change",function() {
+	var filterName = $(this).data("filter"),
+		filterVal = $(this).val();
+	
+	if (filterVal == "") {
+		delete filtersObject[filterName];
+	} else {
+		filtersObject[filterName] = filterVal;
+	}
+	
+	var filters = "";
+	
+	for (var key in filtersObject) {
+	  	if (filtersObject.hasOwnProperty(key)) {
+			filters += "[data-"+key+"='"+filtersObject[key]+"']";
+	 	}
+	}
+	
+	if (filters == "") {
+		$(".product").show();
+	} else {
+		$(".product").hide();
+		$(".product").hide().filter(filters).show();
+	}
 });
 
-loadMore.init();
+//on search form submit
+$("#search-form").submit(function(e) {
+	e.preventDefault();
+	var query = $("#search-form input").val().toLowerCase();
 
-function mySearchProduct() {
-  // Declare variables
-  var input, filter, ul, li;
-  input = document.getElementById("cariProduk");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("list-produk");
-  li = ul.getElementsByTagName("li");
+	$(".product").hide();
+	$(".product").each(function() {
+		var kategori = $(this).data("kategori").toLowerCase();
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (let i = 0; i < li.length; i++) {
-    if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
+		if (kategori.indexOf(query) > -1) {
+			$(this).show();
+		}
+	});
+});
+
+$(document).ready(function(){
+
+      var list = $("#products .product");
+      var numToShow = 4;
+      var button = $("#produk-lainnya");
+      var numInList = list.length;
+      list.hide();
+      if (numInList > numToShow) {
+        button.show();
+      }
+      list.slice(0, numToShow).show();
+
+      button.click(function(){
+          var showing = list.filter(':visible').length;
+          list.slice(showing - 1, showing + numToShow).fadeIn();
+          var nowShowing = list.filter(':visible').length;
+          if (nowShowing >= numInList) {
+            button.hide();
+          }
+      });
+
+});
