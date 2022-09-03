4b908686657eb6813fb4fef8d97992db700b437d
maspannn <cahbagus080@gmail.com>
2022-09-03 10:51:08 +0700

    Mas Pannn

--- smartani/assets/js/budidaya.js
+++ smartani/assets/js/budidaya.js
@@ -389,24 +389,3 @@ $(document).ready(function(){
       }
   });
 });
-
-$(document).ready(function(){
-  var list = $("#rekomendasi-tanaman #artikel-rekomendasi");
-  var numToShow = 4;
-  var button = $("#tanaman-lainnya");
-  var numInList = list.length;
-  list.hide();
-  if (numInList > numToShow) {
-    button.show();
-  }
-  list.slice(0, numToShow).show();
-
-  button.click(function(){
-      var showing = list.filter(':visible').length;
-      list.slice(showing - 1, showing + numToShow).fadeIn();
-      var nowShowing = list.filter(':visible').length;
-      if (nowShowing >= numInList) {
-        button.hide();
-      }
-  });
-});
