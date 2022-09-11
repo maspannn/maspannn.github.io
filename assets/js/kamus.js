e32eefd2ef3c7389771d0553fb6309fbc7af2aa3
maspannn <cahbagus080@gmail.com>
2022-09-11 10:11:44 +0700

    Mas Pannn

--- assets/js/kamus.js
+++ assets/js/kamus.js
@@ -22409,3 +22409,24 @@ $(function() {
       });
   });
 });
+
+$(document).ready(function(){
+  var list = $("#list-kamus .word");
+  var numToShow = 30;
+  var button = $("#kamus-lainnya");
+  var numInList = list.length;
+  list.hide();
+  if (numInList > numToShow) {
+    button.show();
+  }
+  list.slice(0, numToShow).show();
+
+  button.click(function(){
+      var showing = list.filter(':visible').length;
+      list.slice(showing - 1, showing + numToShow).fadeIn();
+      var nowShowing = list.filter(':visible').length;
+      if (nowShowing >= numInList) {
+        button.hide();
+      }
+  });
+});
