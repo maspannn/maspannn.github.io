b63e5c9d49af077bf8c598fcca52480c4c93010e
maspannn <cahbagus080@gmail.com>
2022-09-30 19:27:00 +0700

    Mas Pannn

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -333,24 +333,3 @@ $(function() {
       });
   });
 });
-
-$(document).ready(function(){
-  var list = $("#list-artikel #artikel");
-  var numToShow = 12;
-  var button = $("#artikel-lainnya");
-  var numInList = list.length;
-  list.hide();
-  if (numInList > numToShow) {
-    button.show();
-  }
-  list.slice(0, numToShow).show();
-
-  button.click(function(){
-      var showing = list.filter(':visible').length;
-      list.slice(showing - 1, showing + numToShow).fadeIn();
-      var nowShowing = list.filter(':visible').length;
-      if (nowShowing >= numInList) {
-        button.hide();
-      }
-  });
-});
