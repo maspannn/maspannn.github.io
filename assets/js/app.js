3926970b8334c112eaf127752afb2924e4e225ad
maspannn <cahbagus080@gmail.com>
2023-02-10 09:44:46 +0700

    Update Movie

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,94 @@
 var data = [
     {
-        "title": "The Locksmith (2023)",
-        "poster": "f5f93bd1d158e2a53e651",
+        "title": "Dear David (2023)",
+        "poster": "b96aeb63959b20d018fcb",
         "tahun": 2023,
-        "link": "https://dood.yt/e/wmr0elgz2p1w",
-        "kategori": "Barat"
+        "link": "https://dood.yt/e/f1ie3hj1f3ls",
+        "kategori": "Indonesia"
     },
     {
-        "title": "Alcarras (2023)",
-        "poster": "a703ced3228a812c68f8c",
-        "tahun": 2023,
-        "link": "https://dood.yt/e/u38rh4qio44o",
-        "kategori": "Barat"
+        "title": "Iblis Dalam Kandungan (2022)",
+        "poster": "cb63cb12994e20972cb19",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/2eczckhdo0h7",
+        "kategori": "Indonesia"
     },
     {
-        "title": "Little Dixie (2023)",
-        "poster": "d797a748098d2168d28c8",
-        "tahun": 2023,
-        "link": "https://dood.yt/e/c4lifkawia8j",
-        "kategori": "Barat"
+        "title": "Wengi Anak Mayit (2018)",
+        "poster": "edc0e8fda4b8273ba5a29",
+        "tahun": 2018,
+        "link": "https://dood.yt/e/z2saow88txb9",
+        "kategori": "Indonesia"
     },
     {
-        "title": "Baby Ruby (2023)",
-        "poster": "4d1a06191974bf7a82787",
-        "tahun": 2023,
-        "link": "https://dood.yt/e/lo7ynecawx90",
-        "kategori": "Barat"
+        "title": "Mendarat Darurat (2022)",
+        "poster": "e203b37937dd68d65ccea",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/ebtkdic2dpx1",
+        "kategori": "Indonesia"
     },
     {
-        "title": "Blood (2023)",
-        "poster": "4e128eff5c78d65b3a801",
-        "tahun": 2023,
-        "link": "https://dood.yt/e/l0219e8d90yn",
-        "kategori": "Barat"
+        "title": "12 Cerita Glen Anggara (2022)",
+        "poster": "b973f703095dce0ebb837",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/lme485b28taz",
+        "kategori": "Indonesia"
     },
     {
-        "title": "Condor’s Nest (2023)",
-        "poster": "aead7140eb8f142b57af6",
+        "title": "Jailangkung: Sandekala (2022)",
+        "poster": "fb839de00a1ca8aff016f",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/tm2gb1aeroc9",
+        "kategori": "Indonesia"
+    },
+    {
+        "title": "Scandal Makers (2023)",
+        "poster": "359f6e19cf134e8e7d054",
         "tahun": 2023,
-        "link": "https://dood.yt/e/ca0f6f8x8p9z",
-        "kategori": "Barat"
+        "link": "https://dood.yt/e/wiaes0tg8ggp",
+        "kategori": "Indonesia"
+    },
+    {
+        "title": "The Day Before The Wedding (2023)",
+        "poster": "028e9788b57a747ab3ab2",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/vokoshcx69g0",
+        "kategori": "Indonesia"
+    },
+    {
+        "title": "Layar (2023)",
+        "poster": "b68574e64f8b47c059840",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/5vhha04jgfcl",
+        "kategori": "Indonesia"
+    },
+    {
+        "title": "La Querida (2023)",
+        "poster": "5fec63445c441f10c11ca",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/f6iy3crtwnir",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "10 Day Lover (2023)",
+        "poster": "a2228a5bc72d4e9f831bd",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/1mwjy9ytlhw3",
+        "kategori": "Korea"
+    },
+    {
+        "title": "Mang Kanor (2023)",
+        "poster": "33d4847ffb7383cd124dc",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/k578gshkj135",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "Nightbird (2023)",
+        "poster": "e8ad4e2a0e13e6813bfe5",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/2ud03d65m4ra",
+        "kategori": "Filipina"
     },
     {
         "title": "Plane (2023)",
@@ -70,38 +119,10 @@ var data = [
         "kategori": "Barat"
     },
     {
-        "title": "Mang Kanor (2023)",
-        "poster": "33d4847ffb7383cd124dc",
-        "tahun": 2023,
-        "link": "https://dood.yt/e/k578gshkj135",
-        "kategori": "Filipina"
-    },
-    {
         "title": "Boso Dos (2023)",
         "poster": "960f688cb84d0e8c57fdf",
         "tahun": 2023,
         "link": "https://dood.yt/e/uy58k8p1bc7n",
-        "kategori": "Filipina"
-    },
-    {
-        "title": "Nightbird (2023)",
-        "poster": "e8ad4e2a0e13e6813bfe5",
-        "tahun": 2023,
-        "link": "https://dood.yt/e/2ud03d65m4ra",
-        "kategori": "Filipina"
-    },
-    {
-        "title": "Tag-init (2023)",
-        "poster": "21b5721c029e021a85cfa",
-        "tahun": 2023,
-        "link": "https://dood.yt/e/cazxm81lsy7a",
-        "kategori": "Filipina"
-    },
-    {
-        "title": "Panibugho (2023)",
-        "poster": "0f4758641d926906f8041",
-        "tahun": 2023,
-        "link": "https://dood.yt/e/6j8ucnrvure7",
         "kategori": "Filipina"
     },
     {
@@ -201,6 +222,377 @@ var data = [
         "tahun": 2023,
         "link": "https://dood.yt/e/uzegvxskpesw",
         "kategori": "Filipina"
+    },
+    {
+        "title": "Tag-init (2023)",
+        "poster": "21b5721c029e021a85cfa",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/cazxm81lsy7a",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "Panibugho (2023)",
+        "poster": "0f4758641d926906f8041",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/6j8ucnrvure7",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "Expensive Candy (2022)",
+        "poster": "aae5cc4d230648410b61b",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/p9c9bqig5eze",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "An Affair to Forget (2022)",
+        "poster": "c5f918ff352e271023442",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/lh0sdwfivtm7",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "Bugso (2022)",
+        "poster": "b71bf37848d6691cfcb7e",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/u57lbq0johw5",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "Laruan (2022)",
+        "poster": "8d2416abd5af64564fa76",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/irzsbrfzfks4",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "Reroute (2022)",
+        "poster": "de561fa1b2d426584c9d0",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/7vhvvdzavbqw",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "May-December-January (2022)",
+        "poster": "a7d935bd168bf98386819",
+        "tahun": 2022,
+        "link": "https://dood.yt/e/l3rar11ys6hh",
+        "kategori": "Filipina"
+    },
+    {
+        "title": "Wrath of Desire (2020)",
+        "poster": "350424dbd8183442ef1b8",
+        "tahun": 2020,
+        "link": "https://dood.yt/e/ien1h6ox8m5r",
+        "kategori": "China"
+    },
+    {
+        "title": "My Sex Doll (2020)",
+        "poster": "3d587f33bad6aad0ed5c7",
+        "tahun": 2020,
+        "link": "https://dood.yt/e/fzf73hnxzqtk",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Thai Female Secretary (2020)",
+        "poster": "d5830d3bd2aee82696f22",
+        "tahun": 2020,
+        "link": "https://dood.yt/e/fbq5q05yhui9",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Paradise Inn Phuket: The Taste of College Students (2020)",
+        "poster": "663eb4347651542efcf00",
+        "tahun": 2020,
+        "link": "https://dood.yt/e/hge4z03ei9g4",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Her Deep Love Affair (2017)",
+        "poster": "afc4bec3ecb60d6959cbc",
+        "tahun": 2017,
+        "link": "https://dood.yt/e/tgv6si12p2dc",
+        "kategori": "Korea"
+    },
+    {
+        "title": "Net I Die (2017)",
+        "poster": "5e957db9b6feb8ae3aa36",
+        "tahun": 2017,
+        "link": "https://dood.yt/e/1r9jxh6b02xw",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Are You Coming?! – City of Angels (2017)",
+        "poster": "36c77d0db42f1beaca29a",
+        "tahun": 2017,
+        "link": "https://dood.yt/e/0ndkonuwroz8",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Ten Promises About Love (2016)",
+        "poster": "15b42c3d77630d2ca9be5",
+        "tahun": 2016,
+        "link": "https://dood.yt/e/1ua3dgt49f8h",
+        "kategori": "China"
+    },
+    {
+        "title": "Lover Sex Addiction (2016)",
+        "poster": "957a464aebab42cfd8b90",
+        "tahun": 2016,
+        "link": "https://dood.yt/e/gjtfnybpe0j4",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Lazy Hazy Crazy (2015)",
+        "poster": "14ad18735aff96bbb43fd",
+        "tahun": 2015,
+        "link": "https://dood.yt/e/e3podlxs4cw4",
+        "kategori": "China"
+    },
+    {
+        "title": "Mae Bia (2015)",
+        "poster": "bc1164430533d7b62c6c9",
+        "tahun": 2015,
+        "link": "https://dood.yt/e/rpibj3e5nrza",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Tiger Woman (2015)",
+        "poster": "734182a96172d5719a8bd",
+        "tahun": 2015,
+        "link": "https://dood.yt/e/p87v14krdh9n",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "S for Sex, S for Secrets (2014)",
+        "poster": "100416227bf975651de57",
+        "tahun": 2014,
+        "link": "https://dood.yt/e/krbjynbgeua7",
+        "kategori": "China"
+    },
+    {
+        "title": "Enthralled (2014)",
+        "poster": "062bda7dea9e2e987f563",
+        "tahun": 2014,
+        "link": "https://dood.yt/d/7e6ryl52f74y",
+        "kategori": "China"
+    },
+    {
+        "title": "Spell (2014)",
+        "poster": "a983551802933afae6ef9",
+        "tahun": 2014,
+        "link": "https://dood.yt/e/78rfonm155tv",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Jan Dara: The Finale (2013)",
+        "poster": "228d5386d80d9fc95d4d4",
+        "tahun": 2013,
+        "link": "https://dood.yt/e/kob7g9hlvjif",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Roommate (2013)",
+        "poster": "7d430f024185228a5f124",
+        "tahun": 2013,
+        "link": "https://dood.yt/e/rcswktoxry2q",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Room 65 (2013)",
+        "poster": "7b59381ff91f9d7a1d8b6",
+        "tahun": 2013,
+        "link": "https://dood.yt/e/4rb1i0nx3v94",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Due West: Our Sex Journey (2012)",
+        "poster": "8a5936b01c854b1e7093a",
+        "tahun": 2012,
+        "link": "https://dood.yt/e/51ou6n3hr8l9",
+        "kategori": "China"
+    },
+    {
+        "title": "Ruk Khom Khom Nai Rarn Khanom Cake (2012)",
+        "poster": "8b0d941f3ea50688fec7d",
+        "tahun": 2012,
+        "link": "https://dood.yt/e/rohu6ldzomex",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Jan Dara: The Beginning (2012)",
+        "poster": "554a42336416d7fe8e784",
+        "tahun": 2012,
+        "link": "https://dood.yt/e/m0jfvufy2ev6",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Sam Sao Sex Bomb (2012)",
+        "poster": "ade1cd62138057e651bad",
+        "tahun": 2012,
+        "link": "https://dood.yt/e/cbp1tsmga6wu",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Nang Bang Ngao (2012)",
+        "poster": "166739bf0472926ddbc15",
+        "tahun": 2012,
+        "link": "https://dood.yt/e/zy5ivb5gnhm7",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Ron Rak Tawan Duad (2011)",
+        "poster": "a3716ffb942121e64f8e2",
+        "tahun": 2011,
+        "link": "https://dood.yt/e/8goqv0p2e2dc",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "3-D Sex and Zen: Extreme Ecstasy (2011)",
+        "poster": "fc7551921242e744f85d1",
+        "tahun": 2011,
+        "link": "https://dood.yt/e/0dvcimgwh071",
+        "kategori": "China"
+    },
+    {
+        "title": "Side Line (2011)",
+        "poster": "cc45a0978bf0bc595e580",
+        "tahun": 2011,
+        "link": "https://dood.yt/e/odi9fq8kj7w9",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Ghost Widow (2011)",
+        "poster": "a3d95caa62553c74eb736",
+        "tahun": 2011,
+        "link": "https://dood.yt/e/ri78c2swnqb1",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Nakthotsao Phi hian (2011)",
+        "poster": "781413bea0725e40c91b9",
+        "tahun": 2011,
+        "link": "https://dood.yt/e/f68687ey39or",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Ii Lamyong (2011)",
+        "poster": "1a30c819877bd88039094",
+        "tahun": 2011,
+        "link": "https://dood.yt/e/63piofoglgbx",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Rak Tae Faen Mai (2011)",
+        "poster": "fac97cae0cee350914b5a",
+        "tahun": 2011,
+        "link": "https://dood.yt/e/ywexge3bw2ki",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "Roti Thirak (2010)",
+        "poster": "47b87e411aa0f278b87f5",
+        "tahun": 2010,
+        "link": "https://dood.yt/e/v0ktofo8p0j7",
+        "kategori": "Thailand"
+    },
+    {
+        "title": "The Forbidden Legend Sex And Chopsticks 2 (2009)",
+        "poster": "553b4d6bb39636aa73d5a",
+        "tahun": 2008,
+        "link": "https://dood.yt/e/zui9s8ioifq9",
+        "kategori": "China"
+    },
+    {
+        "title": "The Forbidden Legend: Sex & Chopsticks (2008)",
+        "poster": "bcbcf275a2449cf89e12c",
+        "tahun": 2008,
+        "link": "https://dood.yt/e/81v4cingncgi",
+        "kategori": "China"
+    },
+    {
+        "title": "A Chinese Torture Chamber Story II (1998)",
+        "poster": "fc37abb62d613c2a15ee2",
+        "tahun": 1998,
+        "link": "https://dood.yt/e/zg2zhf6brjyy",
+        "kategori": "China"
+    },
+    {
+        "title": "Island of Greed (1997)",
+        "poster": "19874c73e8eef032fd2aa",
+        "tahun": 1997,
+        "link": "https://dood.yt/e/34sb13zitfwm",
+        "kategori": "China"
+    },
+    {
+        "title": "Don’t Tell My Partner (1997)",
+        "poster": "f2afa9c97c352a1851068",
+        "tahun": 1997,
+        "link": "https://dood.yt/e/fm4af6m1txcu",
+        "kategori": "China"
+    },
+    {
+        "title": "Sex and Zen II (1996)",
+        "poster": "2e7c36fd47777235c2f00",
+        "tahun": 1996,
+        "link": "https://dood.yt/e/iuafc5cvo13c",
+        "kategori": "China"
+    },
+    {
+        "title": "A Chinese Torture Chamber Story (1994)",
+        "poster": "c4afc376d8bccf8cc8eee",
+        "tahun": 1994,
+        "link": "https://dood.yt/e/6w0bvyc2j6zu",
+        "kategori": "China"
+    },
+    {
+        "title": "Sex and Zen (1991)",
+        "poster": "2aa93f2cca26085e2a581",
+        "tahun": 1991,
+        "link": "https://dood.yt/e/syicf8tc6cl1",
+        "kategori": "China"
+    },
+    {
+        "title": "The Locksmith (2023)",
+        "poster": "f5f93bd1d158e2a53e651",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/wmr0elgz2p1w",
+        "kategori": "Barat"
+    },
+    {
+        "title": "Alcarras (2023)",
+        "poster": "a703ced3228a812c68f8c",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/u38rh4qio44o",
+        "kategori": "Barat"
+    },
+    {
+        "title": "Little Dixie (2023)",
+        "poster": "d797a748098d2168d28c8",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/c4lifkawia8j",
+        "kategori": "Barat"
+    },
+    {
+        "title": "Baby Ruby (2023)",
+        "poster": "4d1a06191974bf7a82787",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/lo7ynecawx90",
+        "kategori": "Barat"
+    },
+    {
+        "title": "Blood (2023)",
+        "poster": "4e128eff5c78d65b3a801",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/l0219e8d90yn",
+        "kategori": "Barat"
+    },
+    {
+        "title": "Condor’s Nest (2023)",
+        "poster": "aead7140eb8f142b57af6",
+        "tahun": 2023,
+        "link": "https://dood.yt/e/ca0f6f8x8p9z",
+        "kategori": "Barat"
     },
     {
         "title": "KSI: In Real Life (2023)",
